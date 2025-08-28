1904a4d81b27329a93399be7a4d62d70c87847fb
JStalynB <estalinxk3@gmail.com>
2025-08-28 11:25:09 -0500

    busqueda torres

--- proyecto-alertas/assets/js/main.js
+++ proyecto-alertas/assets/js/main.js
@@ -1,6 +1,4 @@
-// ==========================
-// Alertas de Dynatrace
-// ==========================
+
 
 // ========== API DE DYNATRACE ==========
 
@@ -80,20 +78,7 @@ async function handleNewAlerts(problems) {
       startTimeCell.textContent = new Date(problem.startTime).toLocaleString();
       row.appendChild(startTimeCell);
 
-      //enlace a la URL del problema
-      // const linkCell = document.createElement('td');
-      // const link = document.createElement('a');
-      // link.href = `https://xyc10065.live.dynatrace.com/#problemdetails;gtf=-2h;gf=all;pid=${problem.problemId}`;
-      // link.textContent = 'Ver detalle';
-      // link.target = '_blank';
-      // linkCell.appendChild(link);
-      // row.appendChild(linkCell);
 
-
-      // Enriquecimiento csv manual de conocimiento
-      // ==========================
-      // agregar para ver la solucion 
-             // <strong>Solución:</strong> ${match["Solución"] || 'N/A'}<br>
       const extraInfoCell = document.createElement('td');
       if (match) {
         extraInfoCell.innerHTML = `
@@ -166,17 +151,17 @@ async function handleNewAlerts(problems) {
       // ==========================
     });
 
-  if (problems.length > 0) playAudioAlert();
+//  if (problems.length > 0) playAudioAlert();
 }
 
 // ==========================
 // playAudioAlert
 // ==========================
 
-function playAudioAlert() {
-  const audio = document.getElementById("alertSound");
-  if (audio) audio.play().catch(e => console.error("Error al reproducir audio:", e));
-}
+// function playAudioAlert() {
+//   const audio = document.getElementById("alertSound");
+//   if (audio) audio.play().catch(e => console.error("Error al reproducir audio:", e));
+// }
 
 
 
@@ -252,36 +237,7 @@ function readCSV() {
   fetch('data/data.csv')
     .then(response => response.text())
 
-    // mostrar el contenido del CSV en una tabla con nuevo botón  de pestaña nueva
-    
-    // .then(csv => {
-    //   const rows = csv.split(/\r?\n(?=(?:[^"]*"[^"]*")*[^"]*$)/).filter(r => r.trim() !== '');
-    //   const table = document.getElementById('csvTable');
-    //   const thead = table.querySelector('thead');
-    //   const tbody = table.querySelector('tbody');
-    //   tbody.innerHTML = '';
-
-    //   const headers = rows[0].split(';');
-    //   const headerRow = document.createElement('tr');
-    //   headers.forEach(h => {
-    //     const th = document.createElement('th');
-    //     th.textContent = h.trim();
-    //     headerRow.appendChild(th);
-    //   });
-    //   thead.innerHTML = '';
-    //   thead.appendChild(headerRow);
-
-    //   rows.slice(1).forEach(rowText => {
-    //     const row = document.createElement('tr');
-    //     const cells = rowText.split(/;(?![^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$)/);
-    //     cells.forEach(cell => {
-    //       const td = document.createElement('td');
-    //       td.textContent = cell.replaceAll('"', '').trim();
-    //       row.appendChild(td);
-    //     });
-    //     tbody.appendChild(row);
-    //   });
-    // });
+   
 }
 
 
@@ -299,6 +255,7 @@ async function loadInventarioTorresCSV() {
   });
   return data;
 }
+
 // Mostrar/ocultar panel lateral
 const searchIcon = document.getElementById('searchIcon');
 const searchPanel = document.getElementById('searchPanel');
@@ -355,7 +312,41 @@ document.getElementById('searchBtn').addEventListener('click', async () => {
   } else {
     resultDiv.innerHTML = `<p>No se encontró información para <strong>${query}</strong>.</p>`;
   }
+
+// Cargar inventario torres
+const inventariocsvData = await loadInventarioTorresCSV();
+const matchesinv = inventariocsvData.filter(d =>
+  d["Nombre componente/instancia"] &&
+  d["Nombre componente/instancia"].toLowerCase().includes(query.toLowerCase())
+);
+
+if (matchesinv.length > 0) {
+  resultDiv.innerHTML += `
+    <h3>Resultados en Inventario Torres para: ${query}</h3>
+    ${matchesinv.map(item => `
+      <div>
+        <strong>Torre:</strong> ${item["TORRE"] || 'N/A'}<br>
+        <strong>Componente:</strong> ${item["Nombre componente/instancia"] || 'N/A'}<br><br>
+      </div>
+    `).join('')}
+  `;
+
+  // Resaltar filas en la tabla que tengan coincidencias
+  const affectedRows = document.querySelectorAll('#alertTable tbody tr');
+  affectedRows.forEach(row => {
+    const rowText = row.textContent.toLowerCase();
+    if (matchesinv.some(item => rowText.includes(item["Nombre componente/instancia"].toLowerCase()))) {
+      row.classList.add('highlight-row');
+    }
+  });
+} else {
+  resultDiv.innerHTML += `<p>No se encontró información en Inventario Torres para <strong>${query}</strong>.</p>`;
+}
+   
+  
 });
+
+
 
 
 window.onload = async () => {
